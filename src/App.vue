02a7548861c2ed3b805c1670efae954709367671
kacMadushan <markuptutorials@gmail.com>
2026-05-20 02:58:01 +0530

    Add developer row item

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col">
         <div class="flex items-center justify-between border-b border-s-gray-100 p-4">
         <h2 class="text-black-heading text-4xl">Developers</h2>
-        <button class="bg-s-gray-300 text-black-heading rounded-full cursor-pointer px-6 py-3 font-medium">Create New</button>
+        <button class="bg-s-gray-100 text-black-heading rounded-full cursor-pointer px-6 py-3 font-medium">Create New</button>
       </div>
       <div class="flex items-center justify-between gap-x-8 p-4">
         <div class="w-full">
@@ -25,7 +25,35 @@
           </li>
         </ul>
       </div>
+      <div class="flex flex-col gap-y-2 p-4">
+        <div class="flex items-center rounded-lg justify-between p-4 border border-s-gray-100">
+          <div class="flex items-center gap-x-3">
+            <div class="size-12 bg-s-gray-200 rounded-full"></div>
+            <div>
+              <h4 class="font-medium text-lg text-black-heading">Harry Potter</h4>
+              <span class="block text-s-gray-500 text-sm font-medium">Front-End Developer</span>
+            </div>
+          </div>
+          <div>
+            <button class="border border-s-gray-200 text-black-heading cursor-pointer font-medium px-4 py-2 rounded-full">View Profile</button>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    developerList: [
+      {
+        id: '1',
+        name: 'Harry Potter',
+
+      }
+    ]
+  })
+}
+</script>
