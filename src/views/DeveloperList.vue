4bb16fc4d3af2d2cfbadad1fdde3a8a1c58fa45f
kacMadushan <markuptutorials@gmail.com>
2026-05-22 00:47:39 +0530

    Set developerlist types

--- src/views/DeveloperList.vue
+++ src/views/DeveloperList.vue
@@ -1,3 +1,33 @@
+<script lang="ts">
+import type { Developer } from '@/types';
+import { defineComponent } from 'vue';
+
+interface DeveloperListData {
+  developerList: Developer[];
+}
+
+export default defineComponent({
+  data: (): DeveloperListData => ({
+    developerList: [
+      {
+        id: '1',
+        name: 'Chamika Madushan',
+        email: 'kachamika@gmail.com',
+        title: 'Senior Frontend Developer',
+        headline: 'Senior Frontend Developer | Vue/Nuxt/TypeScript',
+        employmentType: 'Full-time',
+        location: 'Colombo',
+        skills: [
+          { id: '1', label: 'Vue.js' },
+          { id: '2', label: 'Pinia' },
+          { id: '3', label: 'TypeScript' },
+        ],
+      },
+    ],
+  }),
+});
+</script>
+
 <template>
   <div>Developer list</div>
 </template>
