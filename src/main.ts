e0c175a255a30ee760ab1beffaf6d62d4084e830
kacMadushan <markuptutorials@gmail.com>
2026-05-22 00:14:40 +0530

    Reset project root

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-const app = createApp(App)
-app.mount('#app')
+const app = createApp(App);
+app.mount('#app');
